bd22e49226d45b09137530409d19ca5b7eb29962
Viktor <vitosbat@gmail.com>
2021-02-02 02:05:03 +0700

    New arch: added uimanager and start menu buttons handlers

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -5,23 +5,29 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
- //   private Button button;
- //   private GameManager gameManager;
+	private Button button;
+	private GameManager gameManager;
 
- //   public int difficulty;
+	public int difficulty;
 
- //   // Start is called before the first frame update
- //   void Start()
- //   {
- //       button = GetComponent<Button>();
- //       gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+	// Start is called before the first frame update
+	void Start()
+	{
+		button = GetComponent<Button>();
+		
+		//gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
- //       button.onClick.AddListener(SetDifficulty);
- //   }
+		//button.onClick.AddListener(SetDifficulty);
+	}
 
- //   // Set difficulty and start the gameplay
- //    void SetDifficulty()
-	//{
- //       gameManager.StartGame(difficulty);
-	//}
+	public void PushDown()
+	{
+		Debug.Log("Button was pushed");
+	}
+
+	// Set difficulty and start the gameplay
+	void SetDifficulty()
+	{
+		//gameManager.StartGame(difficulty);
+	}
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //public Button startGameButton;
+
+    public void GameStartButtonDownHandler()
+	{
+		Debug.Log("Start Button down handler");
+	}
+
+	public void OptionsButtonDownHandler()
+	{
+		Debug.Log("Options Button down handler");
+	}
+
+}
