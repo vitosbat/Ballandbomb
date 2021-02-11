97b2e51b6ab0edea05645cb579ad5df55c0adbda
Viktor <vitosbat@gmail.com>
2021-02-11 18:13:05 +0700

    Moved spawn logic from target prefab script to Level Manager Script

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,51 +11,80 @@ using UnityEngine;
 
 public class LevelManager : Singleton<LevelManager>
 {
-    GameManager gameManager;
+	GameManager gameManager;
 
-    public LevelDataSO levelData;
+	public LevelDataSO levelData;
 
-    private List<GameObject> targets;
+	private List<GameObject> targets;
 
-    //float spawnRate = 0.5f;
+	private float xSpawnRange = 6.0f;
+	private float ySpawnPos = -2.0f;
+	private float maxTorque = 10.0f;
+	private float minSpeed = 11.0f;
+	private float maxSpeed = 14.0f;
 
-    
-    void Start()
-    {
-        gameManager = GameManager.Instance;
 
-        targets = levelData.Targets;
-        
-        StartCoroutine(SpawnTarget());
-    }
+	void Start()
+	{
+		gameManager = GameManager.Instance;
+
+		targets = levelData.Targets;
+
+		StartCoroutine(SpawnTarget());
+	}
 
 	IEnumerator SpawnTarget()
-	{        
-        while (gameManager.CurrentGameState == GameManager.GameState.GAMEPLAY)
+	{
+		while (gameManager.CurrentGameState == GameManager.GameState.GAMEPLAY)
 		{
 			yield return new WaitForSeconds(levelData.SpawnRate);
+			
 			int targetIndex = Random.Range(0, targets.Count);
-            Instantiate(targets[targetIndex]);
+			CreateTarget(targets[targetIndex]);
 		}
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	void CreateTarget(GameObject targetObject)
+	{
+		Vector3 position = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnPos);
+		GameObject target = Instantiate(targetObject, position, transform.rotation);
+		//Quaternion rotation = Random.Range(-maxTorque, maxTorque);
+		
+		Rigidbody targetRb = target.GetComponent<Rigidbody>();
 
-        // Victory condition
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("You win!");
-            gameManager.UpdateState(GameManager.GameState.ENDLEVEL);
-        }
+		targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        // Defeat condition
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Debug.Log("You lost.");
-            gameManager.UpdateState(GameManager.GameState.ENDLEVEL);
-        }
+		targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
-    }
+	}
+
+	Vector3 RandomForce()
+	{
+		return Vector3.up * Random.Range(minSpeed, maxSpeed);
+	}
+
+	float RandomTorque()
+	{
+		return Random.Range(-maxTorque, maxTorque);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		// Victory condition
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			Debug.Log("You win!");
+			gameManager.UpdateState(GameManager.GameState.ENDLEVEL);
+		}
+
+		// Defeat condition
+		if (Input.GetKeyDown(KeyCode.L))
+		{
+			Debug.Log("You lost.");
+			gameManager.UpdateState(GameManager.GameState.ENDLEVEL);
+		}
+
+	}
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,47 +5,24 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
 	private GameManager gameManager;
-	private Rigidbody targetRb;
-
-	private float minSpeed = 11.0f;
-	private float maxSpeed = 14.0f;
-
-	private float maxTorque = 10.0f;
-
-	private float xSpawnRange = 6.0f;
-
-	private float ySpawnPos = -2.0f;
-
-	public int pointValue;
+	
 	public ParticleSystem explosionParticle;
-
-	// Start is called before the first frame update
+	
 	void Start()
 	{
 		gameManager = GameManager.Instance;
-		
-		targetRb = GetComponent<Rigidbody>();
-
-		transform.position = RandomSpawnPos();
-
-		targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
-
-		targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 	}
 
 
 	private void OnMouseDown()
 	{
-		// TODO: add the checking for GameState is GAMEPLAY.
 		if (gameManager.CurrentGameState == GameManager.GameState.GAMEPLAY)
 		{
 			Destroy(gameObject);
 			Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
 		}
-
 		// gameManager.UpdateScore(pointValue); event?
 		// OnTargetDestroy.Invoke(pointValue); ?
-
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -59,18 +36,4 @@ public class Target : MonoBehaviour
 		}
 	}
 
-	Vector3 RandomForce()
-	{
-		return Vector3.up * Random.Range(minSpeed, maxSpeed);
-	}
-
-	float RandomTorque()
-	{
-		return Random.Range(-maxTorque, maxTorque);
-	}
-
-	Vector3 RandomSpawnPos()
-	{
-		return new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnPos);
-	}
 }
