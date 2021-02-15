63c189e120ecbbb6ee953a351e227add4df406a6
Viktor <vitosbat@gmail.com>
2021-02-15 19:13:17 +0700

    Removed LifeSlider prefab from every scene to UIManager prefab

--- Assets/Scripts/UI/UILifeSlider.cs
+++ Assets/Scripts/UI/UILifeSlider.cs
@@ -5,8 +5,25 @@ using UnityEngine.UI;
 
 public class UILifeSlider : MonoBehaviour
 {
+	GameManager gameManager;
+	LevelManager levelManager;
+
 	[SerializeField] Slider lifeSlider;
 
+
+	private void Start()
+	{
+		gameObject.SetActive(false);
+		gameManager = GameManager.Instance;
+		levelManager = LevelManager.Instance;
+		gameManager.OnGameStateChanged.AddListener(GameStateChangedHandler);
+		levelManager.OnScoreChangesEvent.AddListener(ScoreChangesHandler);
+	}
+
+	private void GameStateChangedHandler(GameManager.GameState currentGameState, GameManager.GameState previousGameState)
+	{
+		gameObject.SetActive(currentGameState == GameManager.GameState.GAMEPLAY);
+	}
 
 	public void ScoreChangesHandler(int value)
 	{
