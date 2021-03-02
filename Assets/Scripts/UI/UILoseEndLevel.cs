4d353fe1930cc0cc8af04c091ba4814b0593e19a
Viktor <vitosbat@gmail.com>
2021-03-02 18:34:07 +0700

    Added score result message on the EndGame Screen

--- Assets/Scripts/UI/UILoseEndLevel.cs
+++ Assets/Scripts/UI/UILoseEndLevel.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class UILoseEndLevel : MonoBehaviour
 {
 	GameManager gameManager;
 
+	[SerializeField] private PlayerSO playerData;
+	
 	private void Start()
 	{
 		gameObject.SetActive(false);
 		gameManager = GameManager.Instance;
 		gameManager.OnGameStateChanged.AddListener(GameStateChangedHandler);
+
 	}
+
 
 	private void GameStateChangedHandler(GameManager.GameState currentGameState, GameManager.GameState previousGameState)
 	{
 		gameObject.SetActive(currentGameState == GameManager.GameState.ENDLEVEL_LOSE);
+		if (currentGameState == GameManager.GameState.ENDLEVEL_LOSE)
+		{
+			string endText = "Good job, " + playerData.PlayerName + "!\n Your score is: " + playerData.PlayerResultScore;
+			
+			transform.Find("EndText").GetComponent<TextMeshProUGUI>().text = endText;
+		}
 	}
 
 }
